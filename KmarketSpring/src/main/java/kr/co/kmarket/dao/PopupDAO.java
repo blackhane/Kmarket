cb3eae199926c676942d83c84ae16da1d315a28d
blackhane <why_not_cry@naver.com>
2023-02-23 08:58:31 +0900

    2023-02-23

---
+++ KmarketSpring/src/main/java/kr/co/kmarket/dao/PopupDAO.java
@@ -0,0 +1,26 @@
+package kr.co.kmarket.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import kr.co.kmarket.vo.MemberVO;
+import kr.co.kmarket.vo.OrderItemVO;
+import kr.co.kmarket.vo.ReviewVO;
+
+@Repository
+@Mapper
+public interface PopupDAO {
+	
+	//판매자정보
+	public MemberVO selectSeller(String company);
+	
+	//수취확인
+	public int updateOrdComplete(String ordNO);
+	
+	//상품리뷰
+	public int insertReview(ReviewVO vo);
+	
+	//주문상세
+	public OrderItemVO selectOrderInfo(String prodNO);
+	
+}
